d9ed259e27cef6012649636cec718e90cf6eae17
wndrbrwn <jyh7678@hotmail.com>
2024-07-25 09:22:00 +0900

    TEST9

---
+++ SolidityTest/SolidityTest9.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.2 < 0.9.0;
+
+contract Test9{
+/*
+흔히들 비밀번호 만들 때 대소문자 숫자가 각각 1개씩은 포함되어 있어야 한다 
+등의 조건이 붙는 경우가 있습니다. 그러한 조건을 구현하세요.
+
+입력값을 받으면 그 입력값 안에 대문자, 
+소문자 그리고 숫자가 최소한 1개씩은 포함되어 있는지 여부를 
+알려주는 함수를 구현하세요.
+*/
+function Password(string memory password) public pure returns (bool) {
+        bytes memory passwordBytes = bytes(password);
+        bool Uppercase;
+        bool Lowercase;
+        bool Number;
+
+        for (uint i = 0; i < passwordBytes.length; i++) {
+            bytes1 char = passwordBytes[i];
+            
+            if (char >= 0x41 && char <= 0x5A) {
+                Uppercase = true;
+            } else if (char >= 0x61 && char <= 0x7A) {
+                Lowercase = true;
+            } else if (char >= 0x30 && char <= 0x39) {
+                Number = true;
+            }
+
+            if (Uppercase && Lowercase && Number) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
